6972116bb018f531f3c81a695a3fa2035582d317
mrjohnming <john.ngai@hotmail.com>
2022-03-05 15:13:16 -0500

    Exercises 28-35

---
+++ ex30.rb
@@ -0,0 +1,28 @@
+# Exercise 30: Else and If
+# https://learnrubythehardway.org/book/ex30.html
+
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people
+  puts "We should take the cars."
+elsif cars < people
+  puts "We should not take the cars."
+else
+  puts "We can't decide."
+end
+
+if trucks > cars
+  puts "That's too many trucks."
+elsif trucks < cars
+  puts "Mayne we could take the trucks."
+else
+  puts "We still can't decide."
+end
+
+if people > trucks
+  puts "Alright, let's just take the trucks."
+else
+  puts "Fine, let's stay home then."
+end
---
+++ ex34.rb
@@ -0,0 +1,28 @@
+# Exercise 34: Accessing Elements of Arrays
+# https://learnrubythehardway.org/book/ex34.html
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1.
+animals[1]
+
+# The third (3rd) animal.
+animals[2]
+
+# The first (1st) animal.
+animals[0]
+
+# The animal at 3.
+animals[3]
+
+# The fifth (5th) animal.
+animals[4]
+
+# The animal at 2.
+animals[2]
+
+# The sixth (6th) animal.
+animals[5]
+
+# The animal at 4.
+animals[4]
